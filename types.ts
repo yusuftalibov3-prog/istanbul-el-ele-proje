e6125c6787a778594707f6f59ccba16475f3d273
yusuftalibov3-prog <yusuftalibov3@gmail.com>
2026-02-11 17:23:24 +0500

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,4 +1,3 @@
-
 export enum UserRole {
   STUDENT = 'Öğrenci',
   SHOPKEEPER = 'Esnaf',
@@ -10,6 +9,7 @@ export interface SolidarityMessage {
   fullName: string;
   phone: string;
   email: string;
+  district: string; // Vercel'in beklediği eksik parça buydu
   message: string;
   role: UserRole;
   createdAt: number;
